827f6a7a723dc291c8d0a63ded57064d893cc59f
Dan Haddix <dan6992@hotmail.com>
2017-07-26 16:06:58 -0400

    1.6.11

--- UserInterface/pyTivoTray.py
+++ UserInterface/pyTivoTray.py
@@ -12,7 +12,7 @@ import json
 from threading import Timer
 from Icons import TrayIcon
 
-versionString = '1.6.9'
+versionString = '1.6.11'
 version = versionString.split('.')
 
 showDesktopOnStart = False
